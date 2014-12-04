f37697eaff9500d8d30e71e8c3ecb1f6283e528b
gulisashvili <levan.gulisashvili@gmail.com>
2014-12-04 22:08:06 +0400

    skeleton app ready for production

--- app.js
+++ app.js
@@ -4,9 +4,20 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var Task = require('./server/models/tasks');
 
+
+
 var app = express();
 
-mongoose.connect('mongodb://localhost/todo-app')
+app.set('env', process.env.NODE_ENV = process.env.NODE_ENV || 'development'); 
+
+// connect to database based on enviroment
+if(app.get('env') == 'development') {
+  mongoose.connect('mongodb://localhost/todo-app');
+} else {
+  mongoose.connect('mongodb://levanigls:milan@ds053160.mongolab.com:53160/todo-app');
+}
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'server/views'));
@@ -25,8 +36,9 @@ app.get('/partials/:partianName', function(req, res) {
 
 // Router
 app.get('*', function(req, res) {
-  res.render('base/layout', { title: "To Do App" });
+  res.render('base/index', { title: "To Do App" });
 });
+
 
 // development error handler
 // will print stacktrace
